e06f143b36b7af4f47e40e725b1459d76eb37a1c
GregDuhamel <gregory.duhamel@outlook.com>
2017-07-08 12:38:34 +0200

    Fix POD

--- ZooMonitor.pl
+++ ZooMonitor.pl
@@ -142,25 +142,25 @@ __END__
 
 =head1 NAME
 
-	ZooMonitor - Monitor ZooKeeper using 4 letter words as described in ZooKeeper documentation
+ZooMonitor - Monitor ZooKeeper using 4 letter words as described in ZooKeeper documentation
 
 =head1 VERSION
 
-	This documentation refers to ZooMonitor version 1.02.
-	
+This documentation refers to ZooMonitor version 1.02.
+
 =head1 USAGE
 
-	ZooMonitor --zkhost=zookeeper-naming-uat01 --zkport=10000 --flw=mntr
-	
-	ZooMonitor --zkhost=zookeeper-naming-uat01,zookeeper-naming-uat02,zookeeper-naming-uat03 --zkport=10000 --flw=mntr
-	
+ZooMonitor --zkhost=zookeeper-naming-uat01 --zkport=10000 --flw=mntr
+
+ZooMonitor --zkhost=zookeeper-naming-uat01,zookeeper-naming-uat02,zookeeper-naming-uat03 --zkport=10000 --flw=mntr
+
 =head1 REQUIRED ARGUMENTS
 
-	--zkhost
-	
-	--zkport
-	
-	--flw
+--zkhost
+
+--zkport
+
+--flw
 
 =head1 OPTIONS
 
@@ -168,25 +168,26 @@ __END__
 
 =item B<--help>
 
-	Print a brief help message and exit.
-	
+Print a brief help message and exit.
+
 =item B<--zkport/-p>
 
-	ZooKeeper remote port. Mandatory.
+ZooKeeper remote port. Mandatory.
 
 =item B<--zkhost/-h>
-	
-	ZooKeeper remote host. Mandatory.
+
+ZooKeeper remote host. Mandatory.
 
 =item B<--flw/-c>
 
-	ZooKeeper Commands: The Four Letter Words. Mandatory.
+ZooKeeper Commands: The Four Letter Words. Mandatory.
 
 =over	
 
 conf
 
 	Print details about serving configuration.
+
 cons
 
 	List full connection/session details for all clients connected to this server.
@@ -194,6 +195,7 @@ cons
 crst
 
 	Reset connection/session statistics for all connections.
+
 dump
 
 	Lists the outstanding sessions and ephemeral nodes. This only works on the leader.
@@ -208,43 +210,50 @@ ruok
 
 	A response of "imok" does not necessarily indicate that the server has joined the quorum, just that the server process is active and bound to the specified client port.
 	Use "stat" for details on state wrt quorum and client connection information.
+
 srst
 
 	Reset server statistics.
+
 srvr
 
 	Lists full details for the server.
+
 stat
 
 	Lists brief details for the server and connected clients.
+
 wchs
 
 	Lists brief information on watches for the server.
+
 wchc
 
 	Lists detailed information on watches for the server, by session.
 	This outputs a list of sessions(connections) with associated watches (paths).
 	Note, depending on the number of watches this operation may be expensive (ie impact server performance), use it carefully.
+
 wchp
 
 	Lists detailed information on watches for the server, by path. This outputs a list of paths (znodes) with associated sessions.
 	Note, depending on the number of watches this operation may be expensive (ie impact server performance), use it carefully.
+
 mntr
 
 	Outputs a list of variables that could be used for monitoring the health of the cluster.
-	
+
 =back
 
 =back
 
 =head1 DESCRIPTION
-	
-	This script will monitor ZooKeeper using The Four Letter Words.
-	
+
+This script will monitor ZooKeeper using The Four Letter Words.
+
 =head1 EXIT STATUS
 
-	OK_NAGIOS    => 0
-	WARN_NAGIOS  => 1
-	ERROR_NAGIOS => 2
+OK_NAGIOS    => 0
+WARN_NAGIOS  => 1
+ERROR_NAGIOS => 2
 
 =cut
