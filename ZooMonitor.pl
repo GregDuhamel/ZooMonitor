a24b92be8608039ae5277272a8fa8b6a674f238f
Gregory Duhamel <gregory.duhamel@outlook.com>
2017-07-10 20:15:00 +0200

    Add functions
    1) Add sendCommand
    2) Add processCommandOnZooHosts
    3) Add parseResult

--- ZooMonitor.pl
+++ ZooMonitor.pl
@@ -6,7 +6,6 @@ use IO::Handle;
 use Getopt::Long;
 use Pod::Usage;
 use Net::Ping;
-use Data::Dumper;
 
 BEGIN {
 	use constant {
@@ -83,17 +82,17 @@ sub checkZooHosts {
 				  . localtime()
 				  . "] $host is unreachable and will not be checked.\n";
 
-				#Removes servers that do not respond ($nb must stay at this value):
+			 #Removes servers that do not respond ($nb must stay at this value):
 				splice( @tmp, $nb, 1 );
 			}
 			else {
-				# One element is valid so we push nb to the next value to keep it:
-				  $nb++;
+			  # One element is valid so we push nb to the next value to keep it:
+				$nb++;
 			}
 		}
 		if ( scalar(@tmp) ) {
-			$Options{'zkhost'}     = undef;
-			$Options{'zkhost'}     = @tmp;
+			$Options{'zkhost'}       = undef;
+			$Options{'zkhost'}       = @tmp;
 			$Options{'zkmultihosts'} = TRUE;
 			undef @tmp;
 			undef @ServerList;
@@ -116,21 +115,97 @@ sub checkZooHosts {
 	return (TRUE);
 }
 
+sub sendCommand {
+	my $host = shift;
+	our %Options;
+	my $response;
+
+	# create a connecting socket
+	my $socket = new IO::Socket::INET(
+		PeerHost => $host,
+		PeerPort => $Options{'zkport'},
+		Proto    => 'tcp',
+	);
+
+	unless ($socket) {
+		print STDERR "[ERROR]["
+		  . localtime()
+		  . "] Can't connect to $host on port $Options{'zkport'}.\n";
+		return (FALSE);
+	}
+
+	# auto-flush on socket
+	IO::Handle::autoflush($socket);
+
+	# data to send to a server
+	my $size = $socket->send( $Options{'flw'} );
+
+	unless ( $size && $size > 0 ) {
+		print STDERR "[ERROR]["
+		  . localtime()
+		  . "] Can't send information to $host on port $Options{'zkport'}.\n";
+		return (FALSE);
+	}
+
+	# notify server that request has been sent
+	shutdown( $socket, 1 );
+
+	# receive a response of up to 8096 characters from server
+	$socket->recv( $response, 8096 );
+
+	unless ($response) {
+		print STDERR "[ERROR]["
+		  . localtime()
+		  . "] Can't receive information from : $host on port : $Options{'zkport'} for command : $Options{'flw'}.\n";
+		return (FALSE);
+	}
+
+	$socket->close();
+
+	return ($response);
+}
+
+sub parseResult {
+	our %CommandResult;
+	return(TRUE);
+}
+
 sub processCommandOnZooHosts {
 	our %Options;
-	
-	
-	
+	our %CommandResult;
+
+	if ( $Options{'zkmultihosts'} ) {
+		my @Hosts = $Options{'zkmultihosts'};
+
+		foreach my $host (@Hosts) {
+			unless ( $CommandResult{$host} = sendCommand($host) ) {
+				$CommandResult{$host} = ERROR_NAGIOS;
+			}
+		}
+		parseResult();
+	}
+	else {
+		unless ( $CommandResult{ $Options{'zkhost'} } =
+			sendCommand( $Options{'zkhost'} ) )
+		{
+			print STDERR "[ERROR]["
+		  . localtime()
+		  . "] Can't process command $Options{'flw'}.\n";
+			exit (ERROR_NAGIOS);
+		}
+		parseResult();
+	}
 	return (TRUE);
 }
 
 sub Main {
 	our %Options;
+	our %CommandResult;
 
 	parseOptions();
 
 	checkZooHosts();
-	
+
 	processCommandOnZooHosts();
 
 	exit(0);
@@ -142,25 +217,25 @@ __END__
 
 =head1 NAME
 
-ZooMonitor - Monitor ZooKeeper using 4 letter words as described in ZooKeeper documentation
+	ZooMonitor - Monitor ZooKeeper using 4 letter words as described in ZooKeeper documentation
 
 =head1 VERSION
 
-This documentation refers to ZooMonitor version 1.02.
-
+	This documentation refers to ZooMonitor version 1.02.
+	
 =head1 USAGE
 
-ZooMonitor --zkhost=zookeeper-naming-uat01 --zkport=10000 --flw=mntr
-
-ZooMonitor --zkhost=zookeeper-naming-uat01,zookeeper-naming-uat02,zookeeper-naming-uat03 --zkport=10000 --flw=mntr
-
+	ZooMonitor --zkhost=zookeeper-naming-uat01 --zkport=10000 --flw=mntr
+	
+	ZooMonitor --zkhost=zookeeper-naming-uat01,zookeeper-naming-uat02,zookeeper-naming-uat03 --zkport=10000 --flw=mntr
+	
 =head1 REQUIRED ARGUMENTS
 
---zkhost
-
---zkport
-
---flw
+	--zkhost
+	
+	--zkport
+	
+	--flw
 
 =head1 OPTIONS
 
@@ -168,26 +243,25 @@ ZooMonitor --zkhost=zookeeper-naming-uat01,zookeeper-naming-uat02,zookeeper-nami
 
 =item B<--help>
 
-Print a brief help message and exit.
-
+	Print a brief help message and exit.
+	
 =item B<--zkport/-p>
 
-ZooKeeper remote port. Mandatory.
+	ZooKeeper remote port. Mandatory.
 
 =item B<--zkhost/-h>
-
-ZooKeeper remote host. Mandatory.
+	
+	ZooKeeper remote host. Mandatory.
 
 =item B<--flw/-c>
 
-ZooKeeper Commands: The Four Letter Words. Mandatory.
+	ZooKeeper Commands: The Four Letter Words. Mandatory.
 
 =over	
 
 conf
 
 	Print details about serving configuration.
-
 cons
 
 	List full connection/session details for all clients connected to this server.
@@ -195,7 +269,6 @@ cons
 crst
 
 	Reset connection/session statistics for all connections.
-
 dump
 
 	Lists the outstanding sessions and ephemeral nodes. This only works on the leader.
@@ -210,50 +283,43 @@ ruok
 
 	A response of "imok" does not necessarily indicate that the server has joined the quorum, just that the server process is active and bound to the specified client port.
 	Use "stat" for details on state wrt quorum and client connection information.
-
 srst
 
 	Reset server statistics.
-
 srvr
 
 	Lists full details for the server.
-
 stat
 
 	Lists brief details for the server and connected clients.
-
 wchs
 
 	Lists brief information on watches for the server.
-
 wchc
 
 	Lists detailed information on watches for the server, by session.
 	This outputs a list of sessions(connections) with associated watches (paths).
 	Note, depending on the number of watches this operation may be expensive (ie impact server performance), use it carefully.
-
 wchp
 
 	Lists detailed information on watches for the server, by path. This outputs a list of paths (znodes) with associated sessions.
 	Note, depending on the number of watches this operation may be expensive (ie impact server performance), use it carefully.
-
 mntr
 
 	Outputs a list of variables that could be used for monitoring the health of the cluster.
-
+	
 =back
 
 =back
 
 =head1 DESCRIPTION
-
-This script will monitor ZooKeeper using The Four Letter Words.
-
+	
+	This script will monitor ZooKeeper using The Four Letter Words.
+	
 =head1 EXIT STATUS
 
-OK_NAGIOS    => 0
-WARN_NAGIOS  => 1
-ERROR_NAGIOS => 2
+	OK_NAGIOS    => 0
+	WARN_NAGIOS  => 1
+	ERROR_NAGIOS => 2
 
 =cut
